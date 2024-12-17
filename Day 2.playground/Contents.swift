99bc321e472ce2c395da5e7ae4c8c9ad010202f9
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2024-12-17 15:51:48 +0530

    Day 2

---
+++ Day 2.playground/Contents.swift
@@ -0,0 +1,36 @@
+import Cocoa
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+let number = 120
+
+var gameOver = false
+print(gameOver)
+
+gameOver.toggle()
+print(gameOver)
+
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greetings = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+let luggageCode = "1" + "2" + "3" + "4" + "5"
+
+let quote1 = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
+
+let missionMessage = "Apollo \(number) landed on the moon."
+
+print("5 x 5 is \(5 * 5)")
