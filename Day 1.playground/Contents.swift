fdbf19f945843b1b12c0c3557e9866067fd584ee
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2024-12-17 15:39:48 +0530

    First commit

---
+++ Day 1.playground/Contents.swift
@@ -0,0 +1,76 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+greeting = "Goodbye, playground"
+
+var PlayerName = "Roy"
+print(PlayerName)
+
+PlayerName = "John"
+print(PlayerName)
+
+PlayerName = "Sam"
+print(PlayerName)
+
+let actor = "Denzel Washington"
+let filename = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+print(actor.count)
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+
+print(filename.hasSuffix(".jpg"))
+
+let score = 10
+let reallyBig = 100000000
+let lowerScore = score - 2
+let hightScore = score + 2
+let doubleScore = score * 2
+let halfScore = score / 2
+let squareScore = score * score
+
+print(reallyBig)
+print(lowerScore)
+print(hightScore)
+print(doubleScore)
+print(halfScore)
+print(squareScore)
+
+var counter = 10
+counter += 5
+print(counter)
+
+counter *= 2
+counter /= 2
+counter -= 5
+
+let number = 120
+print(number.isMultiple(of: 2))
+
+let number1 = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 2.0
+let c = a + Int(b)
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
