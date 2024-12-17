b61ee8f9e3297c616197d6f231504824aa2d1e4d
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2024-12-17 17:37:31 +0530

    Day 3

--- Day 3/Day 3.2 .playground/Contents.swift
+++ Day 3/Day 3.2 .playground/Contents.swift
@@ -1,3 +1,28 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+var people = Set<String>()
+people.insert("Denzel Washington")
+people.insert("Tom Cruise")
+people.insert("Nicolas Cage")
+people.insert("Samuel L Jackson")
+print(people)
+
+//enum Weekday {
+//    case monday
+//    case tuesday
+//    case wednesday
+//    case thursday
+//    case friday
+//}
+
+//var day = Weekday.monday
+//day = Weekday.tuesday
+//day = Weekday.friday
+
+enum Weekday {
+    case monday, tuesday, wednesday, thursday, friday
+}
+
+var day = Weekday.monday
+day = .tuesday
+day = .friday
