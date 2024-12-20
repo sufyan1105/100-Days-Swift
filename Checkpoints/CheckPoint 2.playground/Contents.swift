7dc2422113f5c7dc3d620898c1a26ff59384c66e
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2024-12-20 19:35:35 +0530

    Day 5

--- Checkpoints/CheckPoint 2.playground/Contents.swift
+++ Checkpoints/CheckPoint 2.playground/Contents.swift
@@ -1,2 +1,11 @@
 import Cocoa
 
+let array = ["Apple", "banana","apple", "grapes" , "orange", "orange"]
+
+let totalCount = array.count
+
+print("Total count of elements in array is \(totalCount)")
+
+let uniqueCount = Set(array).count
+
+print("Total count of unique elements in array is \(uniqueCount)")
