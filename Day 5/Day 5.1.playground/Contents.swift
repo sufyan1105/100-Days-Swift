ffab8a1c51f2d6933b494406b326ec60bcdbe32b
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2024-12-21 18:52:16 +0530

    Day 5

--- Day 5/Day 5.1.playground/Contents.swift
+++ Day 5/Day 5.1.playground/Contents.swift
@@ -1,3 +1,73 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let age = 16
+
+if age >= 18 {
+    print("You can vote in the next election.")
+}
+
+if age < 18 {
+    print("Sorry, you're too young to vote.")
+}
+
+if age >= 18 {
+    print("You can vote in the next election.")
+} else {
+    print("Sorry, you're too young to vote.")
+}
+
+//if someCondition {
+//    print("This will run if the condition is true")
+//} else {
+//    print("This will run if the condition is false")
+//}
+
+let a = false
+let b = true
+
+if a {
+    print("Code to run if a is true")
+} else if b {
+    print("Code to run if a is false but b is true")
+} else {
+    print("Code to run if both a and b are false")
+}
+
+let temp = 25
+
+if temp > 20 {
+    if temp < 30 {
+        print("It's a nice day.")
+    }
+}
+
+if temp > 20 && temp < 30 {
+    print("It's a nice day.")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+
+if userAge >= 18 || hasParentalConsent == true {
+    print("You can buy the game")
+}
+
+if userAge >= 18 || hasParentalConsent {
+    print("You can buy the game")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path…")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
