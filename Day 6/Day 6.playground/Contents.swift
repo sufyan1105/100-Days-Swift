bacf7029f8efdb4c562d03f747488ac4b78bc820
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2024-12-23 14:22:24 +0530

    Day 6

---
+++ Day 6/Day 6.playground/Contents.swift
@@ -0,0 +1,30 @@
+import Cocoa
+
+let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
+
+for os in platforms {
+    print("Swift works great on \(os).")
+}
+
+for name in platforms {
+    print("Swift works great on \(name).")
+}
+
+for rubberChicken in platforms {
+    print("Swift works great on \(rubberChicken).")
+}
+
+for i in 1...12 {
+    print("5 x \(i) is \(5 * i)")
+}
+
+for i in 1...12 {
+    print("The \(i) times table:")
+
+    for j in 1...12 {
+        print("  \(j) x \(i) is \(j * i)")
+    }
+
+    print()
+}
+
