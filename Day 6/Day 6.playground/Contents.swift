2a17c66580317644d47fc96d313e1570ceaae0b1
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2024-12-23 14:25:13 +0530

    Day 6

--- Day 6/Day 6.playground/Contents.swift
+++ Day 6/Day 6.playground/Contents.swift
@@ -27,4 +27,20 @@ for i in 1...12 {
 
     print()
 }
+for i in 1...5 {
+    print("Counting from 1 through 5: \(i)")
+}
 
+print()
+
+for i in 1..<5 {
+    print("Counting 1 up to 5: \(i)")
+}
+
+var lyric = "Haters gonna"
+
+for _ in 1...5 {
+    lyric += " hate"
+}
+
+print(lyric)
