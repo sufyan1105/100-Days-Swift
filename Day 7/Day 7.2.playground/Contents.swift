8d8bcd83bcc20cee3a02b9601003dfcb2a718587
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2024-12-25 15:40:50 +0530

    Day 7.2

--- Day 7/Day 7.2.playground/Contents.swift
+++ Day 7/Day 7.2.playground/Contents.swift
@@ -1,3 +1,42 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let root = sqrt(169)
+print(root)
+
+//func rollDice() -> Int {
+//    return Int.random(in: 1...6)
+//}
+
+//let result = rollDice()
+//print(result)
+
+//func areLettersIdentical(string1: String, string2: String) -> Bool {
+//    let first = string1.sorted()
+//    let second = string2.sorted()
+//    return first == second
+//}
+//
+//func areLettersIdentical(string1: String, string2: String) -> Bool {
+//    return string1.sorted() == string2.sorted()
+//}
+
+func areLettersIdentical(string1: String, string2: String) -> Bool {
+    string1.sorted() == string2.sorted()
+}
+
+func rollDice() -> Int {
+    Int.random(in: 1...6)
+}
+
+//func pythagoras(a: Double, b: Double) -> Double {
+//    let input = a * a + b * b
+//    let root = sqrt(input)
+//    return root
+//}
+
+func pythagoras(a: Double, b: Double) -> Double {
+    sqrt(a * a + b * b)
+}
+
+let c = pythagoras(a: 3, b: 4)
+print(c)
