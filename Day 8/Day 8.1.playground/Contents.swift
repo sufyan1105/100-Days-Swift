917b05c7ba21a5d7a017bf1d3811fcb023f1ce31
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2024-12-26 14:25:26 +0530

    Day 8.1

--- Day 8/Day 8.1.playground/Contents.swift
+++ Day 8/Day 8.1.playground/Contents.swift
@@ -1,3 +1,19 @@
 import Cocoa
 
+func printTimesTables(for number: Int, end: Int = 12) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(for: 5, end: 20)
+printTimesTables(for: 8)
+
+var characters = ["Lana", "Pam", "Ray", "Sterling"]
+print(characters.count)
+characters.removeAll()
+print(characters.count)
+
+characters.removeAll(keepingCapacity: true)
+
 
