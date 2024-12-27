3a6558699e79ab7f349a3f3f2a818b3fe57241d9
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2024-12-27 19:00:00 +0530

    Day 9.2

--- Day 9/Day 9.2.playground/Contents.swift
+++ Day 9/Day 9.2.playground/Contents.swift
@@ -1,3 +1,47 @@
 import Cocoa
 
+let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
 
+//let captainFirstTeam = team.sorted(by: { (name1: String, name2: String) -> Bool in
+//    if name1 == "Suzanne" {
+//        return true
+//    } else if name2 == "Suzanne" {
+//        return false
+//    }
+//
+//    return name1 < name2
+//})
+//
+//print(captainFirstTeam)
+
+//let captainFirstTeam = team.sorted { name1, name2 in
+//    if name1 == "Suzanne" {
+//        return true
+//    } else if name2 == "Suzanne" {
+//        return false
+//    }
+//
+//    return name1 < name2
+//}
+
+let captainFirstTeam = team.sorted {
+    if $0 == "Suzanne" {
+        return true
+    } else if $1 == "Suzanne" {
+        return false
+    }
+
+    return $0 < $1
+}
+
+//let reverseTeam = team.sorted {
+//    return $0 > $1
+//}
+
+let reverseTeam = team.sorted { $0 > $1 }
+
+let tOnly = team.filter { $0.hasPrefix("T") }
+print(tOnly)
+
+let uppercaseTeam = team.map { $0.uppercased() }
+print(uppercaseTeam)
