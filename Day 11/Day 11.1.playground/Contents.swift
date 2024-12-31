2364c2138e2bcd6f7c36c006b1b27ba2e7cb5123
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2024-12-31 16:30:54 +0530

    Day 11

--- Day 11/Day 11.1.playground/Contents.swift
+++ Day 11/Day 11.1.playground/Contents.swift
@@ -1,3 +1,60 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+
+//Day 11.1
+
+struct BankAccount {
+    private var funds = 0
+
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds >= amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+var account = BankAccount()
+account.deposit(amount: 100)
+let success = account.withdraw(amount: 200)
+
+if success {
+    print("Withdrew money successfully")
+} else {
+    print("Failed to get the money")
+    
+}
+
+//Day 11.2
+
+struct School {
+    static var studentCount = 0
+
+    static func add(student: String) {
+        print("\(student) joined the school.")
+        studentCount += 1
+    }
+}
+
+School.add(student: "Taylor Swift")
+print(School.studentCount)
+
+
+struct AppData {
+    static let version = "1.3 beta 2"
+    static let saveFilename = "settings.json"
+    static let homeURL = "https://www.hackingwithswift.com"
+}
+
+struct Employee {
+    let username: String
+    let password: String
+
+    static let example = Employee(username: "cfederighi", password: "hairforceone")
+}
