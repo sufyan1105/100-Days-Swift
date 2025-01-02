bd193a8df761339457a0e52b837ec564f26d6f47
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-02 15:24:02 +0530

    Day 12

--- Day 12/Day 12.1.playground/Contents.swift
+++ Day 12/Day 12.1.playground/Contents.swift
@@ -11,3 +11,36 @@ class Game {
 var newGame = Game()
 newGame.score += 10
 newGame.score += 120
+
+
+class Employee {
+    let hours: Int
+
+    init(hours: Int) {
+        self.hours = hours
+    }
+}
+
+class Developer: Employee {
+    func work() {
+        print("I'm writing code for \(hours) hours.")
+    }
+}
+
+class Manager: Employee {
+    func work() {
+        print("I'm going to meetings for \(hours) hours.")
+    }
+}
+
+let robert = Developer(hours: 8)
+let joseph = Manager(hours: 10)
+robert.work()
+joseph.work()
+
+override func printSummary() {
+    print("I'm a developer who will sometimes work \(hours) hours a day, but other times spend hours arguing about whether code should be indented using tabs or spaces.")
+}
+
+let novall = Developer(hours: 8)
+novall.printSummary()
