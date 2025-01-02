29d803323980badda866d629048376c596bfc57b
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-02 15:45:37 +0530

    Day 12

--- Day 12/Day 12.2.playground/Contents.swift
+++ Day 12/Day 12.2.playground/Contents.swift
@@ -19,19 +19,46 @@ class Car: Vehicle {
 
 let teslaX = Car(isElectric: true, isConvertible: false)
 
-class User {
-    var username = "Anonymous"
+//class User {
+//    var username = "Anonymous"
+//
+//    func copy() -> User {
+//        let user = User()
+//        user.username = username
+//        return user
+//    }
+//}
+//var user1 = User()
+//var user2 = user1
+//user2.username = "Taylor"
+//print(user1.username)
+//print(user2.username)
 
-    func copy() -> User {
-        let user = User()
-        user.username = username
-        return user
+class User {
+    let id: Int
+
+    init(id: Int) {
+        self.id = id
+        print("User \(id): I'm alive!")
+    }
+
+    deinit {
+        print("User \(id): I'm dead!")
     }
 }
-var user1 = User()
-var user2 = user1
-user2.username = "Taylor"
-print(user1.username)
-print(user2.username)
 
+for i in 1...3 {
+    let user = User(id: i)
+    print("User \(user.id): I'm in control!")
+}
+var users = [User]()
 
+for i in 1...3 {
+    let user = User(id: i)
+    print("User \(user.id): I'm in control!")
+    users.append(user)
+}
+
+print("Loop is finished!")
+users.removeAll()
+print("Array is clear!")
