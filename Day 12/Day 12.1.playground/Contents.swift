7c8612a0020b61321138690391b67362ef68151b
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-02 15:17:49 +0530

    Day 12

--- Day 12/Day 12.1.playground/Contents.swift
+++ Day 12/Day 12.1.playground/Contents.swift
@@ -1,3 +1,13 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+class Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var newGame = Game()
+newGame.score += 10
+newGame.score += 120
