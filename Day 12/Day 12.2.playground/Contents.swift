490073b2a6d725531cc6e6bda762d6155acd4551
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-02 15:39:20 +0530

    Day 12

--- Day 12/Day 12.2.playground/Contents.swift
+++ Day 12/Day 12.2.playground/Contents.swift
@@ -1,3 +1,37 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+class Vehicle {
+    let isElectric: Bool
+
+    init(isElectric: Bool) {
+        self.isElectric = isElectric
+    }
+}
+
+class Car: Vehicle {
+    let isConvertible: Bool
+
+    init(isElectric: Bool, isConvertible: Bool) {
+        self.isConvertible = isConvertible
+        super.init(isElectric: isElectric)
+    }
+}
+
+let teslaX = Car(isElectric: true, isConvertible: false)
+
+class User {
+    var username = "Anonymous"
+
+    func copy() -> User {
+        let user = User()
+        user.username = username
+        return user
+    }
+}
+var user1 = User()
+var user2 = user1
+user2.username = "Taylor"
+print(user1.username)
+print(user2.username)
+
+
