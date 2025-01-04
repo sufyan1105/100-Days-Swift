f0c5a7e700285e4d86c1c38cc204629a604bac4c
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-04 16:28:48 +0530

    Day 13

--- Day 13/Day 13.2.playground/Contents.swift
+++ Day 13/Day 13.2.playground/Contents.swift
@@ -1,3 +1,27 @@
 import Cocoa
 
+func getRandomNumber() -> Int {
+    Int.random(in: 1...6)
+}
 
+func getRandomBool() -> Bool {
+    Bool.random()
+}
+
+print(getRandomNumber() == getRandomNumber())
+
+//func getRandomNumber() -> Equatable {
+//    Int.random(in: 1...6)
+//}
+
+//func getRandomBool() -> Equatable {
+//    Bool.random()
+//}
+
+func getRandomNumber() -> some Equatable {
+    Int.random(in: 1...6)
+}
+
+func getRandomBool() -> some Equatable {
+    Bool.random()
+}
