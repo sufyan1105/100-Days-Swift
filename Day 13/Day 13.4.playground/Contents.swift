5d4934fdec5536a6864c82b42e3e6dfcb218896f
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-06 15:08:47 +0530

    Day 13

--- Day 13/Day 13.4.playground/Contents.swift
+++ Day 13/Day 13.4.playground/Contents.swift
@@ -1,5 +1,35 @@
 import Cocoa
 
+extension Array {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
+let guests = ["Mario", "Luigi", "Peach"]
+if guests.isNotEmpty {
+    print("Guest count: \(guests.count)")
+}
 
+extension Collection {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
 
+protocol Person {
+    var name: String { get }
+    func sayHello()
+}
 
+extension Person {
+    func sayHello() {
+        print("Hi, I'm \(name)")
+    }
+}
+
+struct Employee: Person {
+    let name: String
+}
+
+let taylor = Employee(name: "Taylor Swift")
+taylor.sayHello()
