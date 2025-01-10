5fae9214af2ee5acfbeb808aad99997e86efea95
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-10 21:31:50 +0530

    Day 14

--- Day 14/Day 14.3.playground/Contents.swift
+++ Day 14/Day 14.3.playground/Contents.swift
@@ -1,5 +1,25 @@
 import Cocoa
 
 
+let captains = [
+    "Enterprise": "Picard",
+    "Voyager": "Janeway",
+    "Defiant": "Sisko"
+]
 
+let new = captains["Serenity", default: "N/A"]
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+let favorite = tvShows.randomElement() ?? "None"
 
+struct Book {
+    let title: String
+    let author: String?
+}
+
+let book = Book(title: "Beowulf", author: nil)
+let author = book.author ?? "Anonymous"
+print(author)
+
+let input = ""
+let number = Int(input) ?? 0
+print(number)
