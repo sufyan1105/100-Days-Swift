ca811ae02cbce1a92a91aa8137d002b963696d23
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-11 12:26:40 +0530

    loops

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,28 +10,41 @@ import SwiftUI
 struct ContentView: View {
     @State private var tapcount = 0
     @State private var name = ""
+    let students = ["Harry", "Ron", "Hermione"]
+    @State private var selectedStudent = "Harry"
     var body: some View {
-        Form{
-            TextField("Enter your name", text: $name)
-            Text("Your name is \(name)")
+        NavigationStack{
+            Form{
+                Picker("Select your student", selection: $selectedStudent){
+                    ForEach(students, id: \.self){
+                        Text($0)
+                    }
+                
+//                ForEach(0..<11){
+//                    Text("Row \($0)")
+                }
+                //            TextField("Enter your name", text: $name)
+                //            Text("Your name is \(name)")
+            }
+            .navigationTitle("Select your student")
+            
+            //        Button("Tap count: \(tapcount)"){
+            //            tapcount += 1
+            //        }
+            //        NavigationStack{
+            //            Form {
+            //                Section {
+            //                    Text("About us")
+            //                }
+            //                Section {
+            //                    Text("Contact")
+            //                    Text("Address")
+            //                }
+            //            }
+            //            .navigationBarTitle("WeSplit")
+            //            .navigationBarTitleDisplayMode(.inline)
+            //        }
         }
-        
-//        Button("Tap count: \(tapcount)"){
-//            tapcount += 1
-//        }
-//        NavigationStack{
-//            Form {
-//                Section {
-//                    Text("About us")
-//                }
-//                Section {
-//                    Text("Contact")
-//                    Text("Address")
-//                }
-//            }
-//            .navigationBarTitle("WeSplit")
-//            .navigationBarTitleDisplayMode(.inline)
-//        }
     }
 }
 
