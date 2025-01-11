4f8bc259263c73b3a2230213a83393bcfdd94ea5
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-11 12:10:46 +0530

    Adding Buttons

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,20 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapcount = 0
     var body: some View {
-        NavigationStack{
-            Form {
-                Section {
-                    Text("About us")
-                }
-                Section {
-                    Text("Contact")
-                    Text("Address")
-                }
-            }
-            .navigationBarTitle("WeSplit")
-            .navigationBarTitleDisplayMode(.inline)
+        
+        Button("Tap count: \(tapcount)"){
+            tapcount += 1
         }
+//        NavigationStack{
+//            Form {
+//                Section {
+//                    Text("About us")
+//                }
+//                Section {
+//                    Text("Contact")
+//                    Text("Address")
+//                }
+//            }
+//            .navigationBarTitle("WeSplit")
+//            .navigationBarTitleDisplayMode(.inline)
+//        }
     }
 }
 
