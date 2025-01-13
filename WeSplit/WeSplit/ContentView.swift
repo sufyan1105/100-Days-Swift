88eb51e546a98ba040853b851827e81aef17dd1a
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-13 12:11:39 +0530

    Wesplit

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,43 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapcount = 0
-    @State private var name = ""
-    let students = ["Harry", "Ron", "Hermione"]
-    @State private var selectedStudent = "Harry"
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+//    @State private var tapcount = 0
+//    @State private var name = ""
+//    let students = ["Harry", "Ron", "Hermione"]
+//    @State private var selectedStudent = "Harry"
     var body: some View {
-        NavigationStack{
-            Form{
-                Picker("Select your student", selection: $selectedStudent){
-                    ForEach(students, id: \.self){
-                        Text($0)
-                    }
-                
-//                ForEach(0..<11){
-//                    Text("Row \($0)")
-                }
-                //            TextField("Enter your name", text: $name)
-                //            Text("Your name is \(name)")
-            }
-            .navigationTitle("Select your student")
-            
-            //        Button("Tap count: \(tapcount)"){
-            //            tapcount += 1
-            //        }
-            //        NavigationStack{
-            //            Form {
-            //                Section {
-            //                    Text("About us")
-            //                }
-            //                Section {
-            //                    Text("Contact")
-            //                    Text("Address")
-            //                }
-            //            }
-            //            .navigationBarTitle("WeSplit")
-            //            .navigationBarTitleDisplayMode(.inline)
-            //        }
-        }
+//        NavigationStack{
+//            Form{
+//                Picker("Select your student", selection: $selectedStudent){
+//                    ForEach(students, id: \.self){
+//                        Text($0)
+//                    }
+//                
+////                ForEach(0..<11){
+////                    Text("Row \($0)")
+//                }
+//                //            TextField("Enter your name", text: $name)
+//                //            Text("Your name is \(name)")
+//            }
+//            .navigationTitle("Select your student")
+//            
+//            //        Button("Tap count: \(tapcount)"){
+//            //            tapcount += 1
+//            //        }
+//            //        NavigationStack{
+//            //            Form {
+//            //                Section {
+//            //                    Text("About us")
+//            //                }
+//            //                Section {
+//            //                    Text("Contact")
+//            //                    Text("Address")
+//            //                }
+//            //            }
+//            //            .navigationBarTitle("WeSplit")
+//            //            .navigationBarTitleDisplayMode(.inline)
+//            //        }
+//        }
     }
 }
 
