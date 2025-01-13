55724e00e0864a9cd38fcdf5dde8369a91952ffd
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-13 16:12:02 +0530

    Navbar

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -13,26 +13,38 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     
     let tipPercentages: [Int] = [10, 15, 20, 25, 30]
-//    @State private var tapcount = 0
-//    @State private var name = ""
-//    let students = ["Harry", "Ron", "Hermione"]
-//    @State private var selectedStudent = "Harry"
+    
     var body: some View {
-        Form{
-            Section{
-                TextField("Check amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-            }
-            Section{
+        NavigationStack {
+            Form {
+                Section {
+                    TextField(
+                        "Check amount",
+                        value: $checkAmount,
+                        format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                    )
+                    .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
                 
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
         }
-//
     }
 }
 
 #Preview {
     ContentView()
 }
+
 
 
 
